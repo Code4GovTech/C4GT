4d80b5398369a35354141276013bc01fbde02a00
Divy Deshmukh <127101632+DivyDeshmukh@users.noreply.github.com>
2025-05-01 14:04:12 +0530

    added user table migration, models & seeder

---
+++ land-demarcation-tracker/backend/src/database/db.config.cjs
@@ -0,0 +1,30 @@
+// this is for the sequelize cli to work with the config file
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
+    },
+}
