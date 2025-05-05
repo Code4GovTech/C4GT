87ff9682ae388ba05f9ee0e4c087b3dc2c984da1
Karan <krnyede@gmail.com>
2025-05-05 21:35:49 +0530

    feat: organize stories under /stories/Components/

---
+++ projects/ui-component-library/src/app/components/input/input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss'
+})
+export class InputComponent {
+
+}
---
+++ projects/ui-component-library/src/app/components/modal/modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+
+}
