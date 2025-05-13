7dada64892205e8b311b22c7ca0de487084af691
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2025-05-13 19:47:03 +0530

    modal component added

--- project/component-library/src/app/modal/modal.component.ts
+++ project/component-library/src/app/modal/modal.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
 })
-export class ModalComponent {}
+export class ModalComponent {
+  @Input() title: string = '';
+  @Input() content: string = '';
+  @Input() confirmText: string = '';
+  @Input() cancelText: string = '';
+  @Input() showCloseBtn = true;
+  @Input() isOpen: boolean = false;
+  @Input() showFooter: boolean = false;
+
+  @Output() close = new EventEmitter<void>();
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onConfirm() {
+    this.confirm.emit();
+  }
+
+  onCancel() {
+    this.cancel.emit();
+  }
+}
