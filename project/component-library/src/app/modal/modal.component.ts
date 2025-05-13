a14196b8a2b6f219750e954654de9dd4ecbc4442
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2025-05-13 20:20:15 +0530

    added reuseable button in modal

--- project/component-library/src/app/modal/modal.component.ts
+++ project/component-library/src/app/modal/modal.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
 })
